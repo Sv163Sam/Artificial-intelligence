ced3bb736e4a1b3f5d9c297c311abaf01037869d
Sv163Sam <Tankruuus@gmail.com>
2024-04-22 20:52:16 +0400

    Add support

--- Dot_Finding.py
+++ Dot_Finding.py
@@ -3,34 +3,51 @@ import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+pose_array = ["NOSE", "LEFT_EYE_INNER", "LEFT_EYE", "LEFT_EYE_OUTER", "RIGHT_EYE_INNER", "RIGHT_EYE", "RIGHT_EYE_OUTER",
+              "LEFT_EAR", "RIGHT_EAR", "MOUTH_LEFT", "MOUTH_RIGHT", "LEFT_SHOULDER", "RIGHT_SHOULDER", "LEFT_ELBOW",
+              "RIGHT_ELBOW", "LEFT_WRIST", "RIGHT_WRIST", "LEFT_PINKY", "RIGHT_PINKY", "LEFT_INDEX", "RIGHT_INDEX",
+              "LEFT_THUMB", "RIGHT_THUMB", "LEFT_HIP", "RIGHT_HIP", "LEFT_KNEE", "RIGHT_KNEE", "LEFT_ANKLE",
+              "RIGHT_ANKLE", "LEFT_HEEL", "RIGHT_HEEL", "LEFT_FOOT_INDEX", "RIGHT_FOOT_INDEX"]
 
-cap = cv2.VideoCapture('GGG/UCF101/PullUps/v_PullUps_g01_c01.avi')
 
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+for i in range(95):
+    strin = "Source/" + str(i+1) + ".mp4"
+    cap = cv2.VideoCapture(strin)
 
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        results = pose.process(image)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            results = pose.process(image)
 
-        """
-        print(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE].x * 512)
-        """
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        if results.pose_landmarks:
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            """
+            with open(filename, 'a') as file:
+                for item in data:
+                file.write(str(item) + '\n')
+            
+            """
 
-        cv2.imshow('Video', image)
+            """
+            print(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE].x)
+            """
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            if results.pose_landmarks:
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+            """
+            cv2.imshow('Video', image)
+            """
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 cap.release()
 cv2.destroyAllWindows()
